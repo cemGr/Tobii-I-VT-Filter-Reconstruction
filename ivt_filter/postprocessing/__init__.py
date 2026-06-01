c5a76b95a0b0ecbdfda2f8a2f1b8cf46c47e0d7a
Cem <66869029+cemGr@users.noreply.github.com>
2026-06-01 17:14:30 +0200

    Refactor postprocessing module exports

--- ivt_filter/postprocessing/__init__.py
+++ ivt_filter/postprocessing/__init__.py
@@ -1,9 +1,13 @@
 """Postprocessing stage: refinement of classified events."""
 
+from .merge_saccades import merge_short_saccade_blocks
+from .pipeline import apply_fixation_postprocessing
 from .merge_fixations import merge_adjacent_fixations
 from .discard_short_fixations import discard_short_fixations
 
 __all__ = [
-    'merge_adjacent_fixations',
-    'discard_short_fixations',
+    "merge_short_saccade_blocks",
+    "apply_fixation_postprocessing",
+    "merge_adjacent_fixations",
+    "discard_short_fixations",
 ]
